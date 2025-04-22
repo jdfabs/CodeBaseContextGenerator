2ab54e09c19c098827623a01530bb501650f87e4
jdfabs <jdfabs1@gmail.com>
2025-04-22 17:27:54 +0100

    feat: extend IContextExtractor interface with additional extraction methods

--- Core/Interfaces/Extractors/IContextExtractor.cs
+++ Core/Interfaces/Extractors/IContextExtractor.cs
@@ -1,8 +1,30 @@
+using CodeBaseContextGenerator.Core.Interfaces.Extractors.MetaData;
+using CodeBaseContextGenerator.Core.Interfaces.Extractors.Relationship;
+using CodeBaseContextGenerator.Core.Interfaces.Extractors.Structure;
 using CodeBaseContextGenerator.Core.Interfaces.Representations;
 
 namespace CodeBaseContextGenerator.Core.Interfaces.Extractors;
 
-public interface IContextExtractor<in TRoot>
+public interface IContextExtractor<in TRoot>:
+    IDocExtractor<TRoot>, 
+    IFullQualifiedNameExtractor<TRoot>,
+    IModifierExtractor<TRoot>,
+    INameExtractor<TRoot>,
+    IPrivacyExtractor<TRoot>,
+    IReturnTypeExtractor<TRoot>,
+    ITypeExtractor<TRoot>,
+    IBaseTypeExtractor<TRoot>,
+    IDependencyExtractor<TRoot>,
+    IImportExtractor<TRoot>,
+    ISourceAnchorExtractor<TRoot>,
+    ITypeUsageExtractor<TRoot>,
+    IAttributeExtractor<TRoot>,
+    ICodeExtractor<TRoot>,
+    IConstructorExtractor<TRoot>,
+    IExceptionExtractor<TRoot>,
+    IFieldExtractor<TRoot>,
+    IMethodExtractor<TRoot>,
+    INestedTypeExtractor<TRoot>,
+    IParameterExtractor<TRoot>
 {
-    protected IReadOnlyCollection<ITypeRepresentation> ExtractContext(TRoot node);
 }
